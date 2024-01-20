a8d8417931f76e26fb838c12c72ce70bd02abe1b
Gabriel Botelho <gabrielbtl98@gmail.com>
2024-01-20 19:44:38 -0300

    Cadastro de Cliente

---
+++ src/main/java/com/gbotelho/brewer/controller/ClienteController.java
@@ -0,0 +1,14 @@
+package com.gbotelho.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClienteController {
+	
+	@RequestMapping(value = "/clientes/novo")
+	public String novo() {
+		return "cliente/CadastroCliente";
+	}
+
+}
